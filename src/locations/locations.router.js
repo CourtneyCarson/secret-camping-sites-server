465b152810182538d8d6dd046393236ac4d85b13
Courtney Carson <cmacarson@gmail.com>
2021-01-07 17:03:42 -0800

    patch to edit own post works

--- src/locations/locations.router.js
+++ src/locations/locations.router.js
@@ -44,6 +44,11 @@ locationRouter
       .catch(next);
   });
 
+
+
+
+
+
 // filter by search word - get request
 locationRouter
   .route('/keyword/:searchTerm')
@@ -66,6 +71,166 @@ locationRouter
   .get((req, res, next) => {
     res.json(res.loc);
   });
+
+/////////////////////// PATCH TO EDIT LOCATION POST ////////////////////////////////////////
+////////// to edit your location posts ////////////////////////////////////////////////////
+locationRouter
+  .route('/:location_id')
+  .all((req, res, next) => {
+    if (isNaN(parseInt(req.params.location_id))) {
+      //if there is an error show it
+      return res.status(404).json({
+        error: {
+          message: `Invalid id`
+        }
+      });
+    }
+
+    //connect to the service to get the data
+    LocationService.getLocationsById(
+      req.app.get('db'),
+      req.params.location_id
+    )
+      .then(location => {
+        if (!location) {
+          //if there is an error show it
+          return res.status(404).json({
+            error: {
+              message: `Location doesn't exist`
+            }
+          });
+        }
+        res.location = location;
+        next();
+      })
+      .catch(next);
+  })
+  .get((req, res, next) => {
+
+    //get each one of the objects from the results and serialize them
+    res.json(res.location);
+  })
+  //relevant
+  .patch(jsonParser, (req, res, next) => {
+
+    //take the input from the user
+    const {
+      title,
+      completed
+    } = req.body;
+    const locationToUpdate = {
+      title,
+      completed
+    };
+
+    //validate the input by checking the length of the locationToUpdate object to make sure that we have all the values
+    const numberOfValues = Object.values(locationToUpdate).filter(Boolean).length;
+    if (numberOfValues === 0) {
+      //if there is an error show it
+      return res.status(400).json({
+        error: {
+          message: `Request body must content either 'title' or 'completed'`
+        }
+      });
+    }
+
+    //save the input in the db
+    LocationService.updateLocation(
+      req.app.get('db'),
+      req.params.location_id,
+      locationToUpdate
+    )
+      .then(updatedLocation => {
+
+        //get each one of the objects from the results and serialize them
+        res.status(200).json(updatedLocation);
+      })
+      .catch(next);
+  });
+
+
+  /////////////////////////////// SAVE ANOTHER USERS POST TO YOUR ACCOUNT - WILL DO BY UTILIZING USER ID //////////////////////////////////////
+////save another users posts to your
+locationRouter
+  .route('/user/:user_id')
+  .all((req, res, next) => {
+    if (isNaN(parseInt(req.params.user_id))) {
+      //if there is an error show it
+      return res.status(404).json({
+        error: {
+          message: `Invalid id`
+        }
+      });
+    }
+
+    //connect to the service to get the data
+    LocationService.getUsersById(
+      req.app.get('db'),
+      req.params.user_id
+    )
+      .then(user => {
+        if (!user) {
+          //if there is an error show it
+          return res.status(404).json({
+            error: {
+              message: `User doesn't exist`
+            }
+          });
+        }
+        res.user = user;
+        next();
+      })
+      .catch(next);
+  })
+  .get((req, res, next) => {
+
+    //get each one of the objects from the results and serialize them
+    res.json(res.user);
+  })
+  //relevant
+  .patch(jsonParser, (req, res, next) => {
+
+    //take the input from the user
+    const {
+      title,
+      completed
+    } = req.body;
+    const userToUpdate = {
+      title,
+      completed
+    };
+
+    //validate the input by checking the length of the userToUpdate object to make sure that we have all the values
+    const numberOfValues = Object.values(userToUpdate).filter(Boolean).length;
+    if (numberOfValues === 0) {
+      //if there is an error show it
+      return res.status(400).json({
+        error: {
+          message: `Request body must content either 'title' or 'completed'`
+        }
+      });
+    }
+
+    //save the input in the db
+    LocationService.updateLocationUser(
+      req.app.get('db'),
+      req.params.user_id,
+      userToUpdate
+    )
+      .then(updatedUser => {
+
+        //get each one of the objects from the results and serialize them
+        res.status(200).json(updatedUser);
+      })
+      .catch(next);
+  });
+
+
+
+
+
+
+
 
 
 
--- src/locations/locations.service.js
+++ src/locations/locations.service.js
@@ -4,18 +4,8 @@ const LocationService = {
       .select('*')
       .from('locations');
   },
-  getLocationsById(db, loc_id) {
-    return db
-      .from('locations')
-      .select('*')
-      .where({ 'id': loc_id })
-      .first();
-  },
-  // saved to users account:
-  // getLocationsByUser(db, user_id) {
-  //   return db
-  //   .from('locations')
-  // }
+  
+ 
   insertNewLocation(db, newLoc) {
     return db
       .insert(newLoc)
@@ -32,6 +22,46 @@ const LocationService = {
       .from('locations')
       .select('*')
       .where('keyword', 'like', `%${key_word}%`);
+  },
+
+  // update location to save to acct 
+  getLocationsById(db, loc_id) {
+    return db
+      .from('locations')
+      .select('*')
+      .where({ 'id': loc_id })
+      .first();
+  },
+  updateLocation(db, location_id, newLocation) {
+    return db('locations')
+        .update(newLocation, returning = true)
+        .where({
+            id: location_id
+        })
+        .returning('*')
+        .then(rows => {
+            return rows[0]
+        })
+  },
+
+  // save a public post to your account 
+  getUsersById(db, user_id) {
+    return db
+      .from('locations')
+      .select('*')
+      .where({ 'id': user_id })
+      .first();
+  },
+  updateLocationUser(db, user_id, newLocation) {
+    return db('locations')
+      .update(newLocation, returning = true)
+      .where({
+      id: user_id
+      })
+      .returning('*')
+      .then(rows => {
+      return rows[0]
+    })
   },
 };
 
