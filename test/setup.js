d3b696c456ea31fb61ce4f0b4858b3bd4dce0716
Courtney Carson <cmacarson@gmail.com>
2021-01-15 10:02:43 -0800

    auth + comments tests

--- test/setup.js
+++ test/setup.js
@@ -1,4 +1,12 @@
-const { expect } = require('chai')
+process.env.TZ = 'UTC'
+process.env.NODE_ENV = 'test'
+require('dotenv').config()
+
+process.env.TEST_DATABASE_URL = process.env.DATABASE_URL
+
+const {
+    expect
+} = require('chai')
 const supertest = require('supertest')
 
 global.expect = expect
