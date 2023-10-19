95371ffa83f9dccb3dbcfad632f74698516b5992
Courtney Carson <cmacarson@gmail.com>
2023-10-18 18:47:31 -0700

    update host name

--- src/config.js
+++ src/config.js
@@ -2,8 +2,8 @@ module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || 'production',
   JWT_SECRET: process.env.JWT_SECRET || 'change-this-secret',
-  DATABASE_URL: process.env.DATABASE_URL || 'postgres://courtneycarson:Bruno858@/tmp:5432/secret_camping',
-  TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || 'postgres://courtneycarson:Bruno858@/tmp:5432/secret_camping',
+  DATABASE_URL: process.env.DATABASE_URL || 'postgres://courtneycarson:Bruno858@/socket:5432/secret_camping',
+  TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || 'postgres://courtneycarson:Bruno858@/socket:5432/secret_camping',
   API_TOKEN: process.env.API_TOKEN || 'dummy-api-token',
 }
 
