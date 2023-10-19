b7f98571bbbc9cad0202661c6d137856982f4ddf
Courtney Carson <cmacarson@gmail.com>
2023-10-18 18:03:35 -0700

    update node versions

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 module.exports = {
   PORT: process.env.PORT || 8000,
-  NODE_ENV: process.env.NODE_ENV || 'development',
+  NODE_ENV: process.env.NODE_ENV || 'production',
   JWT_SECRET: process.env.JWT_SECRET || 'change-this-secret',
   DATABASE_URL: process.env.DATABASE_URL || 'postgresql://courtneycarson@localhost/secret_camping',
   TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || 'postgresql://courtneycarson@localhost/secret_camping',
