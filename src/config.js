24e9b0043fb0c853c2592c7f4dd1a873971e0727
Courtney Carson <cmacarson@gmail.com>
2023-10-19 12:57:38 -0700

    elephantsql url

--- src/config.js
+++ src/config.js
@@ -2,8 +2,8 @@ module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || 'production',
   JWT_SECRET: process.env.JWT_SECRET || 'change-this-secret',
-  DATABASE_URL: process.env.DATABASE_URL || 'postgresql://courtneycarson:Bruno858@localhost:5432/secret_camping',
-  TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || 'postgresql://courtneycarson:Bruno858@localhost:5432/secret_camping',
+  DATABASE_URL: process.env.DATABASE_URL || 'postgres://xpskjyty:ikKBt_esfB8x3V_252ktQ0V7lOV4u5iH@chunee.db.elephantsql.com/xpskjyty',
+  TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || 'postgres://xpskjyty:ikKBt_esfB8x3V_252ktQ0V7lOV4u5iH@chunee.db.elephantsql.com/xpskjyty',
   API_TOKEN: process.env.API_TOKEN || 'dummy-api-token',
 }
 
